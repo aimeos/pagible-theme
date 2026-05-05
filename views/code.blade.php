244f65d0d9758f0267170e0a36e5bd892463d36b
aimeos <aimeos@aimeos.org>
2026-05-05 11:06:21 +0000

    Improved themes

--- views/code.blade.php
+++ views/code.blade.php
@@ -1,5 +1,1 @@
-@pushOnce('foot')
-<link href="{{ cmstheme($page, 'code.css') }}" rel="stylesheet">
-@endPushOnce
-
 <pre><code class="language-{{ @$data->language?->value }}" dir="ltr">{{ @$data->text }}</code></pre>
